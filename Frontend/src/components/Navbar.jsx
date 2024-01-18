69573f1dfbe0a07d7f5f662c1eb778ea4bdbd8e4
Abdul <abdulshafey24@gmail.com>
2024-01-18 23:45:26 +0530

    Initial commit

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+
+const Navbar = () => {
+  const user = true;
+  return (
+    <div className="flex items-centre justify-between px-6 md:px-[200px] py-4">
+      <h1 className="text-lg qmd:text-xl font-extrabold ">
+        <Link to="/">Blog</Link>
+      </h1>
+      <div className="flex justify-center items-centre space-x-0">
+        <p className="my-2">
+          <BsSearch />
+        </p>
+        <input
+          className="outline-none px-3 py-1"
+          placeholder="Search a post"
+          type="text"
+        ></input>
+      </div>
+      <div className="flex items-centre justify-center space-x-2 md:space-x-4">
+        {user ? (
+          <h3>
+            <Link to="/write">Write</Link>
+          </h3>
+        ) : (
+          <h3>
+            <Link to="/login"></Link>Login
+          </h3>
+        )}
+        {user ? (
+          <h3>Profile</h3>
+        ) : (
+          <h3>
+            <Link to="/register"></Link>Register
+          </h3>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
